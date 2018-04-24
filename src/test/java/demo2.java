23b1ab3165f11672cede6adc339353730ba88c03
sachinvasdev <vasdev@cloudxtension.com>
2018-04-24 22:52:29 +0530

    Test file

---
+++ src/test/java/demo2.java
@@ -0,0 +1,17 @@
+package test.java;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class demo2
+{
+    @Test
+    public void testMethod()
+    {
+        System.out.println("success test");
+        String [] str =null;
+        String s = str[0];
+
+    }
+}
