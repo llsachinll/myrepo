85b84d9da94525a46c6fe24dceff8ae779d8e1b9
sachinvasdev <vasdev@cloudxtension.com>
2018-04-24 21:56:34 +0530

    Test file

---
+++ src/test/java/demo1.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.testng.annotations.Test;
+
+public class demo1
+{
+    @Test(description = "test")
+    public void testMethod()
+    {
+        System.out.println("success");
+    }
+}
